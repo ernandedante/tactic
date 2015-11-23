d1074c96d4e9bffe49d941218b1d1e0dfac69d78
Ernande <eernandee@hotmail.com>
2015-11-23 18:12:21 -0200

    Update connect_up_command.py

--- connect_up_command.py
+++ connect_up_command.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
-'''
-	========================================================================================
-		TACTIC DEV Script Connect to server * 
-			Faz a criacao dos snapshot no trabalho que for setado. 
-			Os nomes do snapshot vao ser nomeados com o nome do proprio render. Assim se 
-			cria um snapsho com uma identidade unica.
-
-			atulizado : 17/11/2015
-	========================================================================================
-'''
+# ==========================================================================================
+#	 		$ Descrition - /
+#
+#					TACTIV DEV SCRIP CONNECT TO server
+#
+#	 Faz a ligacao com o servidor. Ira manda os arquivos que foram selecionados.
+#
+#
+#	 				by:. Ernande dante
+#	
+#													     			 atualizacao: 23/11/2015
+#
+# ==========================================================================================
 
 from searchfiles import TacticSearch
-from pprint import pprint
 import os
 
 ''' Essa classe por enquanto apenas mostra algumas etapas para upar um arquivo.
@@ -29,34 +31,42 @@ class ConnectionTactic():
 		# self.search_type = 'atomo'
 		self.search_type = 'atomo/shot'
 
+		# * * * * * * * * * 
+		self.nameSnapshots()
+
+
 	def nameSnapshots(self):
-		# nameSnap = "NOMEFIGURA_"
 		for index, itens in enumerate(self.paths):
 			# Code recebe o nome que vai ser colocado no snapshot.
 			self.listNameSnapshots.append({ 'code': '', 'description' : itens })	
 			if os.listdir(itens):
 				for i in os.listdir(itens):
-					self.listNameSnapshots[index].update(code=i)
+					# Atualiza o valor da lista com para seta cada nome de arquivos com seu path
+					self.listNameSnapshots[index].update(code=i[:-4])
 		return self.listNameSnapshots	
 
-	def verificar(self):
-		for i in range(len(self.listNameSnapshots)):
-			if server.get_by_code(search_type, self.nameSnapshots()[i]['code'])
-				code_triagem = server.get_by_code(search_type, self.nameSnapshots()[i]['code'])
-				if code_triagem['code']:
-					print str(self.nameSnapshots()[i]['code']) + "codigos"
-				else:
-					return True
+
+	# def verificar(self):
+	# 	if self.listNameSnapshots:
+	# 		for i in range(len(self.listNameSnapshots)):
+	# 			if server.get_by_code(search_type, self.nameSnapshots()[i]['code']):
+	# 				code_triagem = server.get_by_code(search_type, self.nameSnapshots()[i]['code'])
+	# 				if code_triagem['code']:
+	# 					print str(self.nameSnapshots()[i]['code']) + "codigos"
+	# 				else:
+	# 					return True
+
 
 	def creatSnapshot(self):
 		''' Essa funcao deve criar os snapshot, que vao receber arquivos.
 			Para cada aquivo que for updado, teve se ter um snapshot, por
 			tanto ela vai ser usada em loop.'''
-		# 
-		self.nameSnapshots()
+
+		# self.nameSnapshots() # inicializa a nomeacao dos snapshot
 		for index in range(len(self.listNameSnapshots)):
 			result = server.insert(self.search_type, self.listNameSnapshots[index])
 			print "-- loop %s " % result 
+
 
 	def updateSnapshot(self):
 		''' Essa funcao vai fazer o upload dos arquivos'''
@@ -66,7 +76,7 @@ class ConnectionTactic():
 			snapshot_code = server.create_snapshot(search_key, 'model')
 			server.add_file(snapshot_code.get('code'), self.nameSnapshots + '/' + code, mode='upload', create_icon=True)
 
+	
+
 c = ConnectionTactic()
-print c.creatSnapshot()
-
-
+print c.go()
