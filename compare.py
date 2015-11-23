d560448d3978c094a02409b8086ccd3f4f590f86
Ernande <eernandee@hotmail.com>
2015-11-23 18:33:38 -0200

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# ==========================================================================================
+#	 		$ Descrition 	
+#
+#					TACTIV DEV SCRIP
+#
+#	 Sincronizar arquivos, verificar se ja existem, ou se estao em fila de "espera"!  
+#	
+#	 Busca essas informacoes no servidor. Verificar se ja existem.
+#	 	 				
+#	 Fazer uma veredura no servidor do tactic, para veficar e atualizar novos arquivos.
+#
+#
+#						by:. Ernande dante
+#	
+#													     			 atualizacao: 23/11/2015
+#
+# ==========================================================================================
+
+from searchfiles import TacticSearch
+
+def compare():
+	# buscar informacoes do servidor	
+	pass
+
+
+def verifyUp():
+	# Verificar se tem arquivos para serem uploadado.
