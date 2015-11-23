bd781faf9095d8feb11c3e6f1e59f3ffaedb1317
Ernande <eernandee@hotmail.com>
2015-11-23 18:34:10 -0200

    Create report.py

---
+++ report.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# ==========================================================================================
+#	 		$ Descrition 	
+#
+#					TACTIV DEV SCRIP
+#
+#	 Gera um relatorio para cada vez que o processo atualizar, ou upar um novo arquivo  
+#	 
+#
+#						by:. Ernande dante
+#	
+#													     			 atualizacao: 23/11/2015
+#
+# ==========================================================================================
+
+# Documentar cada aquivo, em um padrao que possa ser lido facilmento.
